9ff3c32fdff7e57737c865c4643643616161f6f2
Aneeq Khatri <aneeqabdulsamad5761@gmail.com>
2023-04-02 11:55:16 +0500

    new changes

--- index.js
+++ index.js
@@ -1,32 +1,31 @@
-import express from "express"
-import dotenv from "dotenv"
-import mongoose from "mongoose"
-import authRoutes from "./routes/authRoutes.js"
-import hotelRoutes from "./routes/HotelRoutes.js"
-import roomRoutes from "./routes/RoomRoutes.js"
-import usersRoutes from "./routes/UserRoutes.js"
-import cookieParser from "cookie-parser"
-import  cors from "cors"
-mongoose.set('strictQuery', false)
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import authRoutes from "./routes/authRoutes.js";
+import hotelRoutes from "./routes/HotelRoutes.js";
+import roomRoutes from "./routes/RoomRoutes.js";
+import usersRoutes from "./routes/UserRoutes.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+mongoose.set("strictQuery", false);
 
+const app = express();
+dotenv.config();
 
-const app = express()
-dotenv.config()
+console.log("process.env.MONGO_URI", process.env.MONGO_URI);
+const DB = process.env.MONGO_URI;
 
-console.log("process.env.MONGO_URI",process.env.MONGO_URI)
-const DB = process.env.MONGO_URI
-
-const connect =async ()=>{
+const connect = async () => {
   try {
-   await mongoose.connect(DB)
-   console.log("connected to mongoDB")
+    await mongoose.connect(DB);
+    console.log("connected to mongoDB");
   } catch (error) {
-    throw error
+    throw error;
   }
-} 
-mongoose.connection.on("disconnected",()=>{
-  console.log("mongoDB disconnected")
-})
+};
+mongoose.connection.on("disconnected", () => {
+  console.log("mongoDB disconnected");
+});
 
 // middleWares
 app.use(
@@ -35,30 +34,33 @@ app.use(
     exposedHeaders: ["authorization"], // you can change the headers
     origin: "*",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    preflightContinue: false
+    preflightContinue: false,
   })
 );
-app.use(cookieParser())
-app.use(express.json())
+app.use(cookieParser());
+app.use(express.json());
 
-app.use("/api/v1/auth",authRoutes)
-app.use("/api/v1/users",usersRoutes)
-app.use("/api/v1/hotels",hotelRoutes)
-app.use("/api/v1/rooms",roomRoutes)
+app.use("/", (req, res) => {
+  res.send("Hi from the express server");
+});
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/users", usersRoutes);
+app.use("/api/v1/hotels", hotelRoutes);
+app.use("/api/v1/rooms", roomRoutes);
 
-app.use((err,req,res,next)=>{
+app.use((err, req, res, next) => {
   const errorStatus = err.status || 500;
   const errorMessage = err.message || "Something Went wrong!";
   return res.status(errorStatus).json({
-    success:false,
-    status:errorStatus,
-    message:errorMessage,
-    stack:err.stack
-  })
+    success: false,
+    status: errorStatus,
+    message: errorMessage,
+    stack: err.stack,
+  });
+});
 
-})
-
-app.listen(8000,()=>{
-  connect()
-  console.log("connected to backend")
-})
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  connect();
+  console.log("connected to backend");
+});
